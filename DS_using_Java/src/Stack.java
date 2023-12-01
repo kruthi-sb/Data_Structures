5108b507089f1b35a2bc3906a78fd1c36eda757d
Kruthi S B <kruthi.banakar@gmail.com>
2023-12-01 15:19:02 +0530

    implemented stack in both languages

---
+++ DS_using_Java/src/Stack.java
@@ -0,0 +1,43 @@
+public class Stack {
+    private int maxSize;
+    private int top;
+    private int[] stackArray;
+
+    public Stack(int size) {
+        maxSize = size;
+        stackArray = new int[maxSize];
+        top = -1;
+    }
+
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push element.");
+            return;
+        }
+        stackArray[++top] = value;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot pop element.");
+            return -1;
+        }
+        return stackArray[top--];
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty. Cannot peek element.");
+            return -1;
+        }
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+}
